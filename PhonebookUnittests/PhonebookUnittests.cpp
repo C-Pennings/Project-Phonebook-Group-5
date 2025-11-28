ea66741307df09045f49e6ff93bf0d06414111ac
C-pennings <charliepennings13@gmail.com>
2025-11-28 13:54:12 -0500

    Add project files.

---
+++ PhonebookUnittests/PhonebookUnittests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Phonebook/main.c"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PhonebookUnittests
+{
+	TEST_CLASS(PhonebookUnittests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
