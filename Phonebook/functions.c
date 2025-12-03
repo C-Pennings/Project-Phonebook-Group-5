4c09983118dcfe623796ca657502cfcf0956433a
C-pennings <charliepennings13@gmail.com>
2025-12-03 16:18:39 -0500

    set up stubs for functions.c, header file will be updated when all stubs are tested and verified

--- Phonebook/functions.c
+++ Phonebook/functions.c
@@ -1,2 +1,53 @@
 #include "functions.h"
 
+#include <stdbool.h>
+#include "types.c"
+
+bool append(List*, Entry*);
+bool prepend(List*, Entry*);
+int search(List*, String*, String*);
+bool delete(List*, int);
+bool free_list(List*);
+Entry* step_node(Node*, int);
+Entry* step_back_node(Node*, int);
+Entry* create_entry(String*, String*);
+
+Entry* create_entry(String* name, String* phone_number) {
+	// Implementation goes here
+	return NULL;
+}
+
+bool append(List* list, Entry* entry) {
+	// Implementation goes here
+	return true;
+}
+
+bool prepend(List* list, Entry* entry) {
+	// Implementation goes here
+	return true;
+}
+
+int search(List* list, String* value, String* type) {
+	// Implementation goes here
+	return -1;
+}
+
+bool delete(List* list, int index) {
+	// Implementation goes here
+	return true;
+}
+
+bool free_list(List* list) {
+	// Implementation goes here
+	return true;
+}
+
+Entry* step_node(Node* node, int steps) {
+	// Implementation goes here
+	return NULL;
+}
+
+Entry* step_back_node(Node* node, int steps) {
+	// Implementation goes here
+	return NULL;
+}
