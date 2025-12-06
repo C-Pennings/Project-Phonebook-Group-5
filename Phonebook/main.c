059b2096b78489b4b4a5c9cfd0353750d8f62ba7
TravisKCroft <travis.k.croft@gmail.com>
2025-12-06 12:54:51 -0500

    While loop for main program.

--- Phonebook/main.c
+++ Phonebook/main.c
@@ -18,47 +18,67 @@ int main() {
 	}
 	init_list(contact_list);
 
+	bool continue_program = true;
 
-	printf( BLUE BOLD "~~~Welcome to Your Phone Directory!~~~" RESET "\n");
-	printf("Enter a number corresponding with the action you wish to take.\n");
-	printf(BLUE BOLD "1) " RESET "Add a new contact.\n");
-	printf(BLUE BOLD "2) " RESET "Delete a contact.\n");
-	printf(BLUE BOLD "3) " RESET "Update a contact.\n");
-	printf(BLUE BOLD "4) " RESET "Display contacts.\n"); //Will have a sub-menu
-	printf(BLUE BOLD "5) " RESET "Search for an contact.\n");
-	printf(BLUE BOLD "6) " RESET "Exit program.\n\n");
+	while (continue_program) {
 
-	int menu_input = input_number(1, 6);
+		printf(BLUE BOLD "~~~Welcome to Your Phone Directory!~~~" RESET "\n");
+		printf("Enter a number corresponding with the action you wish to take.\n");
+		printf(BLUE BOLD "1) " RESET "Add a new contact.\n");
+		printf(BLUE BOLD "2) " RESET "Delete a contact.\n");
+		printf(BLUE BOLD "3) " RESET "Update a contact.\n");
+		printf(BLUE BOLD "4) " RESET "Display contacts.\n"); //Will have a sub-menu
+		printf(BLUE BOLD "5) " RESET "Search for an contact.\n");
+		printf(BLUE BOLD "6) " RESET "Exit program.\n\n");
 
-	switch (menu_input) {
-		case 1: //Add a new entry
+		int menu_input = input_number(1, 6);
 
-			printf("\nAdding a new contact...\n");
+		switch (menu_input) {
+			case 1: //Add a new entry
 
-			printf("Enter contact name: ");
-			String* contact_name = input_string();
-			printf("Enter contact phone number: ");
-			String* contact_number = input_string();
+				printf("\nAdding a new contact...\n");
 
-			Entry* new_contact = create_entry(contact_name, contact_number);
-			append(contact_list, new_contact);
+				printf("Enter contact name: ");
+				String* contact_name = input_string();
+				printf("Enter contact phone number: ");
+				String* contact_number = input_string();
 
-			break;
+				Entry* new_contact = create_entry(contact_name, contact_number);
+				append(contact_list, new_contact);
 
-		case 2: //Delete an entry
-			break;
+				break;
 
-		case 3: //Update an entry
-			break;
+			case 2: //Delete an entry
 
-		case 4: //Display entries
-			break;
+				printf("\Deleting a contact...\n");
 
-		case 5: //Search
-			break;
+				break;
 
-		case 6: //exit
-			break;
+			case 3: //Update an entry
+
+				printf("\nUpdating a contact...\n");
+
+				break;
+
+			case 4: //Display entries
+
+				printf("\nDisplay a range or a specific entry?\n");
+				//More menu
+				break;
+
+			case 5: //Search
+
+				printf("\nSearching for a contact...\n");
+
+				break;
+
+			case 6: //exit
+
+				printf("\nExiting program...\n");
+				continue_program = false;
+
+				break;
+		}
 	}
 
 	free_list(contact_list);
