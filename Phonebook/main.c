0fbfdfb675d0efd0578c78d1f4f7ba017160b4a7
TravisKCroft <travis.k.croft@gmail.com>
2025-12-06 12:08:33 -0500

    Get input and evaluate with switch.

--- Phonebook/main.c
+++ Phonebook/main.c
@@ -9,25 +9,33 @@
 int main() {
 	ENABLE_COLORS();
 
-	/*printf(GREEN BOLD "Input Test (1-100)" RESET "\n");
-	int test_str = input_number(1, 100);
-	if (test_str != NULL) {
-		printf("You entered: " CYAN BOLD "%d" RESET "\n", test_str);
-
-	} else {
-		printf(RED BOLD "Failed to read input integer." RESET "\n");
-	}*/
-
 	printf( BLUE BOLD "~~~Welcome to Your Phone Directory!~~~" RESET "\n");
 	printf("Enter a number corresponding with the action you wish to take.\n");
-	printf(BLUE "1)" RESET "Add a new entry.\n");
-	printf(BLUE "2)" RESET "Delete an entry.\n");
-	printf(BLUE "3)" RESET "Update an entry.\n");
-	printf(BLUE "4)" RESET "Display entries.\n"); //Will have a sub-menu
-	printf(BLUE "5)" RESET "Search for an entry.\n");
+	printf(BLUE BOLD "1) " RESET "Add a new entry.\n");
+	printf(BLUE BOLD "2) " RESET "Delete an entry.\n");
+	printf(BLUE BOLD "3) " RESET "Update an entry.\n");
+	printf(BLUE BOLD "4) " RESET "Display entries.\n"); //Will have a sub-menu
+	printf(BLUE BOLD "5) " RESET "Search for an entry.\n\n");
 	//6) Save and load options? (If we want the user to choose what is saved)
 
+	int menu_input = input_number(1, 5);
 
+	switch (menu_input) {
+		case 1: //Add a new entry
+			break;
+
+		case 2: //Delete an entry
+			break;
+
+		case 3: //Update an entry
+			break;
+
+		case 4: //Display entries
+			break;
+
+		case 5: //Search
+			break;
+	}
 
 	return 0;
 }
