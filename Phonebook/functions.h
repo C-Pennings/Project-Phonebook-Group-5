d4cea9b7d008564356034e39a48017259ab16476
JameshWray <jwray2009@conestogac.on.ca>
2025-12-07 12:28:13 -0500

    Added simple continue function Added a contains function, only works for begining of word Added search_all , for linkist thats subset of main list

--- Phonebook/functions.h
+++ Phonebook/functions.h
@@ -5,6 +5,7 @@
 bool append(List*, Entry*);
 bool prepend(List*, Entry*);
 int search(List*, String*, String*);
+void search_all(List*,List*, String*, String*);
 bool delete(List*, int);
 bool free_list(List*);
 Entry* step_node(Node*, int);
@@ -12,3 +13,5 @@ Entry* step_back_node(Node*, int);
 Entry* create_entry(String*, String*);
 void init_list(List* list);
 String* get_search_type();
+bool A_Contains_B(String*,String*);
+void free_temp_list(List*);
